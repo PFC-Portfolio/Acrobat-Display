0ee559738873b140c486c32dc375e6fb51564e0c
PFC-Portfolio <57277427+PFC-Portfolio@users.noreply.github.com>
2019-11-11 13:25:10 -0800

    Demonstration of AxInterop.AcroPDFLib in Windows Forms

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Metrophile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Directory.SetCurrentDirectory(Directory.GetCurrentDirectory().ToString() + "/PDF Repository");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            axAcroPDF1.LoadFile("King County Metro 106.pdf");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            axAcroPDF1.LoadFile("King County Metro 775.pdf");
+        }
+    }
+}
